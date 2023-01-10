23b9c037e3c996803ff1e8e762fd371fc03500fa
Big Daddy Game Dev <bigdaddygamedev@gmail.com>
2023-01-11 12:59:48 +1300

    Terraria Ground Tile Lighting
    Init for Terraria Ground Tile Lighting and Shadows

---
+++ Assets/_GaussianBlur/SimpleBlit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SimpleBlit : MonoBehaviour
+{
+    //[SerializeField]
+    Texture2D _baseTexture;
+
+    //private void Awake()
+    //{
+    //    _baseTexture = toTexture2D(GetComponent<Camera>().targetTexture);
+    //}
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+        //_baseTexture = toTexture2D(GetComponent<Camera>().targetTexture);
+        Graphics.Blit(GetComponent<Camera>().targetTexture, destination);
+    }
+
+    Texture2D toTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
+        // ReadPixels looks at the active RenderTexture.
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
+}
